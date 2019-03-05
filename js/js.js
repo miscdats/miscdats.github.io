1db257e7f2a82e42a610e66b0bc9e142184c960c
deyanira <30326911+miscdats@users.noreply.github.com>
2019-03-05 13:51:39 -0800

    PAGE OVERHAUL

--- js/js.js
+++ js/js.js
@@ -1,13 +1,5 @@
-function pressedForTime() {
-    var today = new Date();
-    var day = today.getDate();
-    var month = today.getMonth() + 1;
-    var year = today.getFullYear();
-    var hour = today.getHours();
-    var minute = today.getMinutes();
-    var second = today.getSeconds();
-    var time =  [hour, minute, second].join(':');
-    var d8 = [year, month, day].join('/');
-    console.log([d8, time].join(' '));
-    alert("Currently: " + [d8, time].join(' '));
-}
+$(function(){
+
+  
+
+});

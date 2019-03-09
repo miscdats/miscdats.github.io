d3cd55352a91b0fecfdfce0eb2ce06fcf000ac39
deyanira <30326911+miscdats@users.noreply.github.com>
2019-03-09 00:23:17 -0800

    change div actions

--- js/js.js
+++ js/js.js
@@ -1,5 +1,10 @@
 $(function(){
 
-  
+  // show div in main when click on nav button
+  $( '#nav_bar a' ).click(function() {
+    var target = $(this).attr('rel');
+    $("#" + target).show().siblings("div").hide();
+  });
+
 
 });

646475a09aad5b2bb564e2abd1a88f6ef7ad3b28
deyanira <30326911+miscdats@users.noreply.github.com>
2019-03-09 19:29:46 -0800

    fx

--- scripts/js.js
+++ scripts/js.js
@@ -6,5 +6,18 @@ $(function(){
     $("#" + target).show().siblings("div").hide();
   });
 
+  // banner fx
+  $( '.glitch-img' ).mgGlitch({
+    destroy: false,
+    glitch: true,
+    scale: true,
+    blend: true,
+    blendModeType: 'hue',
+    glitch1TimeMin: 200,
+    glitch1TimeMax: 400,
+    glitch2TimeMin: 10,
+    glitch2TimeMax: 100,
+    zIndexStart: 5
+  });
 
 });

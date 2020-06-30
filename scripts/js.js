35de56b989abbcf21a68d839e3fea1cdb25c966d
deya <30326911+miscdats@users.noreply.github.com>
2020-06-30 15:22:36 -0700

    Splash: text.

--- scripts/js.js
+++ scripts/js.js
@@ -11,12 +11,19 @@ $(function(){
     document.getElementById( '#thxMsg' ).innerHTML = 'I\'ve got mail! Thanks, you\'ll be hearing back from me soon.';
   };
 
-  // splash screen enter link
-  // $('.enter_link').click(function () {
-  //   $(this).parent('#splashscreen').fadeOut(500);
-  //   // $(this).parent('#wrap').style.display = 'inline';
-  // });
-  
+  // SPLASH ANIMATIONS
+  $( '.glitch-img-splash' ).mgGlitch({
+    destroy: false,
+    glitch: true,
+    scale: true,
+    blend: true,
+    blendModeType: 'hue',
+    glitch1TimeMin: 200,
+    glitch1TimeMax: 400,
+    glitch2TimeMin: 10,
+    glitch2TimeMax: 100,
+    zIndexStart: 5
+  });
 
   function fade(element) {
     var op = 1;  // initial opacity
@@ -33,27 +40,29 @@ $(function(){
   }
 
   setTimeout(function(){ 
-
-    if(typeof(Storage) !== "undefined") {
+    // Do not show splash if can't or already seen
+    if (typeof(Storage) !== "undefined") {
       console.log("Already shown" + sessionStorage.getItem('splashShown'));
 
+      // Display splash, timeout to main
       if( !sessionStorage.getItem('splashShown') || sessionStorage.getItem('splashShown') === null ) {  
         document.getElementById('wrap').style.display = 'none';
-        document.getElementById('splashscreen') .style.display = 'inline';
-        //Display splash
+        document.getElementById('splashscreen').style.display = 'inline';
+
         setTimeout(function(){   
           fade(document.getElementById('splashscreen'));
           // document.getElementById('splash').style.display = 'none'
           // window.location = "https://taupier.dev";
           sessionStorage.setItem('splashShown', true  );
+
           $( '.glitch-img-splash' ).mgGlitch({
             destroy: true,
             glitch: false,
             scale: false,
             blend: false
           });
+
           document.getElementById('wrap').style.display = 'inline';
-          
           $( '.glitch-img' ).mgGlitch({
             destroy: false,
             glitch: true,
@@ -66,7 +75,7 @@ $(function(){
             glitch2TimeMax: 100,
             zIndexStart: 5
           });
-        }, 3000);
+        }, 3500);
         
       } else {
         //Display Main Content
